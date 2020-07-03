d934a3105726f60108f543a0136597eb0e02fd4b
danielkpodo <kpododanielnarh@gmail.com>
2020-07-03 11:35:34 +0000

    finished list

---
+++ python_basics/datatypes/lists/list_unpacking.py
@@ -0,0 +1,6 @@
+# List unpacking enables to assign variables to our list item
+
+a, b, c = [1, 2, 4]  # a = 1, b = 2, c = 4
+
+name, age, address, *other = ["narh", 26, "kasseh", "a", "c", "drink", "dress"]
+print(other)
